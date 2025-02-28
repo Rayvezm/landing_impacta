26a15b888c41d7e703fc209da752475964b8e44c
Rayve Giovanni Zerpa Materán <raymarzerpa45@gmail.com>
2025-02-28 18:10:32 -0400

    Impacta Finalizado 1.1

--- js/main.js
+++ js/main.js
@@ -1,18 +1,26 @@
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'horizontal',
-  loop: true,
+document.addEventListener("DOMContentLoaded", () => {
+  const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
 
-  // If we need pagination
+    // If we need pagination
     pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-  },
+      el: '.swiper-pagination',
+      clickable: true,
+    },
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
 
+  });
+  AOS.init();
+  
+  const formi = document.getElementById('Formi1');
+  formi.addEventListener("submit", () => { 
+    alert('Please enter');
+  });
 });
